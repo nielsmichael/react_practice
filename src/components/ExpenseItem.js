0177ebff9ef35bb47f803ad793fffc65b37a40e6
xXnielsXx <nielsmichael@comcast.net>
2022-04-14 13:44:18 +0900

    Add expense list const and display w/ map

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseCost = 249.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${expenseCost}</div>
+      <div className="expense-item__price">${props.cost}</div>
     </div>
   );
 }
