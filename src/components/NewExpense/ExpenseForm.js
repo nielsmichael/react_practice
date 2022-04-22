658445b6238b4660f695a82046c65f55b9ff36a7
xXnielsXx <nielsmichael@comcast.net>
2022-04-22 23:19:58 +0900

    Add state to expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const updateTitle = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const [enteredAmt, setEnteredAmt] = useState("");
+  const updateAmt = (e) => {
+    setEnteredAmt(e.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState("");
+  const updateDate = (e) => {
+    setEnteredDate(e.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={updateTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={updateAmt} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2021-01-01"
+            max="2023-12-31"
+            onChange={updateDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
